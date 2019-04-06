5e1ca59618554f9437d59588fa1ed831b2702dd7
ArmedKatana <luiz.suzano2010@hotmail.com>
2019-04-06 20:27:56 -0300

    Commit via VSCode

---
+++ src/jogovelha.py
@@ -0,0 +1,14 @@
+def inicializar():
+	tab = []
+	for i in range(3):
+		linha = []
+		for j in range(3):
+			linha.append(".")
+		tab.append(linha)
+	return tab
+
+def main():
+	jogo = inicializar()
+	print(jogo)
+if __name__ == "__main__":
+	main()
